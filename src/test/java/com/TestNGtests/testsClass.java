9dcb426736690257d883147502304c46fb688035
[rmahavenkat] <rmahavenkat@gmail.com>
2025-05-03 22:43:21 -0500

    added testclass and pageobject

--- src/test/java/com/TestNGtests/testsClass.java
+++ src/test/java/com/TestNGtests/testsClass.java
@@ -1,28 +1,22 @@
 package com.TestNGtests;
 
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import com.BaseClass.baseClass;
 import com.pageObject.pageObjectclass;
 
+public class testsClass extends baseClass {
 
-
-public class testsClass extends baseClass{
-	
-
-	 @Test
-	    public void verifyRecipeListClick() throws InterruptedException {
-	        pageObjectclass page = new pageObjectclass(driver,wait);
-	        
-	        page.clickRecipeList();
-	       
-	        driver.navigate().refresh();
-	        Thread.sleep(1000);
-	        page.click_on_recipes();
-	        
-	        
-	        
-	        
-	    }
+	@Test
+	public void verifyRecipeListClick() throws InterruptedException {
+		pageObjectclass page = new pageObjectclass(driver, wait);
+		page.clickRecipeList();
+		driver.navigate().refresh();
+		page.clickRecipeList();
+		//Thread.sleep(1000);
+		page.click_on_recipes();
+		
+	}
 
 }
--- src/test/java/com/pageObject/pageObjectclass.java
+++ src/test/java/com/pageObject/pageObjectclass.java
@@ -1,7 +1,6 @@
 package com.pageObject;
 
 import java.util.List;
-
 import org.openqa.selenium.ElementClickInterceptedException;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
@@ -12,52 +11,79 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class pageObjectclass {
-	 WebDriver driver;
-	    WebDriverWait wait;
+	WebDriver driver;
+	WebDriverWait wait;
 
-	    public pageObjectclass(WebDriver driver,WebDriverWait wait) {
-	        this.driver = driver;
-	        this.wait = wait;
-	        PageFactory.initElements(driver, this);
-	    }
+	@FindBy(xpath = "//a[contains(text(),'Vegetable Manchurian, Veg Manchurian with gravy Re')]")
+	public WebElement VegManchurian;
 
-	    @FindBy(xpath = "//a[text()='Recipes List']")
-	    public WebElement recipes_list;
+	public pageObjectclass(WebDriver driver, WebDriverWait wait) {
+		this.driver = driver;
+		this.wait = wait;
+		PageFactory.initElements(driver, this);
+	}
 
-	    @FindBy(xpath = "//span[text()='Close']")
-	    public WebElement dismiss_btn;
+	@FindBy(xpath = "//a[text()='Recipes List']")
+	public WebElement recipes_list;
 
-	    @FindBy(className = "two-line-text")
-	    public List<WebElement> recipes;
+	/*
+	 * @FindBy(xpath = "//span[text()='Close']") public WebElement dismiss_btn;
+	 */
+	@FindBy(xpath = "//span[text()='Close']")
+	public List<WebElement> dismiss_btns;
 
-	    public void clickRecipeList() {
-	        try {
-	            ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", recipes_list);
-	           // WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-	            wait.until(ExpectedConditions.elementToBeClickable(recipes_list));
-	            recipes_list.click();
-	        } catch (ElementClickInterceptedException e) {
-	            dismiss(); 
-	            clickRecipeList(); 
-	        }
-	    }
+	@FindBy(className = "two-line-text")
+	public List<WebElement> recipes;
 
-	    public void dismiss() {
-	    	
-	            dismiss_btn.click();
-	        }
-	    
+	public void clickRecipeList() {
+		try {
+			((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", recipes_list);
+			// WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+			wait.until(ExpectedConditions.elementToBeClickable(recipes_list));
+			recipes_list.click();
+		} catch (ElementClickInterceptedException e) {
+			System.out.println("Click intercepted, trying to dismiss popup...");
+			dismiss();
+			clickRecipeList();
+			// dismiss();
 
-	    public void click_on_recipes() {
-	           for (int i = 0; i < 24; i++) {
-	            try {
-	                recipes.get(i).click();
-	                driver.navigate().back(); 
-	               // PageFactory.initElements(driver, this); 
-	            } catch (Exception e) {
-	                System.out.println("Skipping index " + i + ": " + e.getMessage());
-	            }
-	        }
-	    }
+		}
+	}
+
+	/*
+	 * public void dismiss() {
+	 * 
+	 * dismiss_btn.click(); }
+	 */
+	public void dismiss() {
+		try {
+			if (!dismiss_btns.isEmpty()) {
+				WebElement btn = dismiss_btns.get(0);
+				wait.until(ExpectedConditions.elementToBeClickable(btn));
+				btn.click();
+				System.out.println("Popup dismissed.");
+			} else {
+				System.out.println("No popup to dismiss.");
+			}
+		} catch (Exception e) {
+			System.out.println("Dismiss failed or popup not present: " + e.getMessage());
+		}
+	}
+
+	/*
+	 * public void click_on_recipes() { for (int i = 1; i < 24; i++) { try {
+	 * recipes.get(i).click(); driver.navigate().back(); //
+	 * PageFactory.initElements(driver, this); } catch (Exception e) {
+	 * System.out.println("Skipping index " + i + ": " + e.getMessage()); } }
+	 */
+
+	public void click_on_recipes() {
+		VegManchurian.click();
+        String url=driver.getCurrentUrl();
+		//String url = "https://www.tarladalal.com/vegetable-manchurian--easy-chinese-recipe--4137r";
+		String recipeId = url.substring(url.lastIndexOf('-') + 1);
+		System.out.println(recipeId);
+
+	}
 
 }
