1a0441a85a7c54d7488bfb5a3eef44043045f426
Rajasri <Rajasri.kr@gmail.com>
2025-05-05 13:50:10 -0500

    Pagination completed

--- src/test/java/com/TestNGtests/RecipeScrapperTest.java
+++ src/test/java/com/TestNGtests/RecipeScrapperTest.java
@@ -1,20 +1,22 @@
 package com.TestNGtests;
 
+import com.BaseClass.baseClass;
 import com.pageObject.HomePage;
 import com.pageObject.RecipeListPage;
 import org.testng.annotations.Test;
 
-public class RecipeScrapperTest {
+public class RecipeScrapperTest  extends baseClass {
 
-    private HomePage homePage;
+    private HomePage homePage = new HomePage();
     private RecipeListPage recipeListPage;
-    private RecipeDatabase database;
+    private RecipeDatabase database = new RecipeDatabase();
 
     @Test
     void scrapeRecipe() {
         homePage.launch();
         homePage.navigateToRecipeList();
 
+        recipeListPage = new RecipeListPage(driver);
         recipeListPage.parseAllRecipesOnAllPages();
 
         database.persistAllRecipes();
