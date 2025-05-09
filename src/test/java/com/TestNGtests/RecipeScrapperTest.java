532cca2edd1480016fec1b1bf644bce1ff15f8f2
rmahavenkat <rmahavenkat@gmail.com>
2025-05-09 15:59:04 -0500

    Update RecipeScrapperTest.java

--- src/test/java/com/TestNGtests/RecipeScrapperTest.java
+++ src/test/java/com/TestNGtests/RecipeScrapperTest.java
@@ -1,7 +1,5 @@
 package com.TestNGtests;
 
-
-import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
@@ -11,24 +9,18 @@ import java.util.concurrent.TimeoutException;
 import com.BaseClass.baseClass;
 import com.pageObject.pageObjectclass;
 
-public class RecipeScrapperTest extends baseClass { 
-   
+public class RecipeScrapperTest extends baseClass {
+
 	pageObjectclass page;
 
-    @Test
-    void scrapeRecipe() throws SQLException, TimeoutException,  IOException {
-       
+	@Test
+	void scrapeRecipe() throws SQLException, TimeoutException, Exception, IOException {
 
-    	 page = new pageObjectclass(driver,wait);
-	        
-    	  page.clickRecipeList();	          
-	        driver.navigate().refresh();
-	        page.removeAds();	        
-	        page.click_on_recipes_with_pagination();
-	      
+		page = new pageObjectclass(driver, wait);
+		page.clickRecipeList();
+		driver.navigate().refresh();
+		page.removeAds();
+		page.click_on_recipes_with_pagination();
 
-       
-
-
-}
+	}
 }
