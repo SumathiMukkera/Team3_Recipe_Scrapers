9421b371b5c536f6ab9728175682164fb3f7e49f
[rmahavenkat] <rmahavenkat@gmail.com>
2025-05-09 15:54:54 -0500

    added food processing table

--- src/test/java/com/pageObject/pageObjectclass.java
+++ src/test/java/com/pageObject/pageObjectclass.java
@@ -45,6 +45,7 @@ public class pageObjectclass {
 	public static final String LFV_ADD = "Lettuce,kale,chard,arugula,spinach,cabbage,pumpkin,sweet potatoes,purple potatoes,yams,turnip,parsnip,karela,bittergourd,beet,carrot,cucumber,red onion,white onion,broccoli,cauliflower,carrot,celery,artichoke,bell pepper,mushroom,tomato,sweet and hot pepper,banana,mango,papaya,plantain,apple,orange,pineapple,pear,tangerine,all berry varieties,all melon varieties,peach,plum,nectarine,Avocado,Amaranth,Rajgira,Ramdana Barnyard,Sanwa,Samvat ke chawal buckwheat,kuttu finger millet,Ragi,Nachni foxtail millet,kangni,kakum kodu,kodon,little millet,moraiyo,kutki,shavan,sama pearl millet,bajra,broom corn millet,chena sorghum,jowar,Lentil,Pulse,Moong dhal,masoor dhal,toor dhal,urd dhal,lobia,rajma,matar,all forms of chana,almond,cashew,pistachio,brazil nut,walnut,pine nut,hazelnut,macadamia nut,pecan,peanut,hemp seed,sun flower seed,sesame seed,chia seed,flax seed";
 	public static final String LFV_TO_ADD = "Butter,Ghee,salmon,mackerel,sardines";
 	public static final String ALLERGY = "Milk,Soy,Egg,Sesame,Peanuts,walnut,almond,hazelnut,pecan,cashew,pistachio,Shell fish,Seafood";
+	public static final String LCHF_ELEMINATE_FOOD_PROCESSING = "raw,steamed, boiled, porched,sauted,airfryed,pan fried";
 
 	List<Recipe> lfvEliminationRecipes = new ArrayList<Recipe>();
 	List<Recipe> allRecipesList = new ArrayList<Recipe>();
@@ -55,9 +56,10 @@ public class pageObjectclass {
 	// List<Recipe> lfvToAvoidRecipes = new ArrayList<Recipe>();
 	List<Recipe> lfvAllergyRecipes = new ArrayList<Recipe>();
 	List<Recipe> lchfAllergyRecipes = new ArrayList<Recipe>();
+	List<Recipe> lchfEliminationRecipesFoodProcessing = new ArrayList<Recipe>();
 
 	String[] tableNames = { "recipes", "LCHFEliminatedRecipe", "lchfAddRecipes", "lfvEliminationRecipes",
-			"lfvAddRecipes", "lfvToAddRecipes", "lfvAllergyRecipes", "lchfAllergyRecipes" };
+			"lfvAddRecipes", "lfvToAddRecipes", "lfvAllergyRecipes", "lchfAllergyRecipes","lchfEliminationRecipesFoodProcessing" };
 
 	@FindBy(xpath = "//a[text()='Recipes List']")
 	public WebElement recipes_list;
@@ -172,18 +174,7 @@ public class pageObjectclass {
 		lfvToAddRecipes = filterRecipes(lfvEliminationRecipes, LFV_TO_ADD, false);
 		lfvAllergyRecipes = filterRecipes(lfvEliminationRecipes, ALLERGY, true);
 		lchfAllergyRecipes = filterRecipes(lchfEliminationRecipes, ALLERGY, true);
-
-		System.out.println("******************************************************************");
-		System.out.println(lfvEliminationRecipes);
-		System.out.println("******************************************************************");
-		System.out.println(lchfEliminationRecipes);
-		System.out.println("******************************************************************");
-		System.out.println(lchfAddRecipes);
-		System.out.println("******************************************************************");
-		System.out.println(lfvAddRecipes);
-		System.out.println("******************************************************************");
-		System.out.println(lfvToAddRecipes);
-		System.out.println("******************************************************************");
+		lchfEliminationRecipesFoodProcessing = filterRecipes(lchfEliminationRecipes,LCHF_ELEMINATE_FOOD_PROCESSING, true);
 
 		insertRecipesIntoTable("recipes", allRecipesList);
 		insertRecipesIntoTable("lfvEliminationRecipes", lfvEliminationRecipes);// insert method to add values to the
@@ -194,6 +185,7 @@ public class pageObjectclass {
 		insertRecipesIntoTable("lfvToAddRecipes", lfvToAddRecipes);
 		insertRecipesIntoTable("lfvAllergyRecipes", lfvAllergyRecipes);
 		insertRecipesIntoTable("lchfAllergyRecipes", lchfAllergyRecipes);
+		insertRecipesIntoTable("lchfEliminationRecipesFoodProcessing",lchfEliminationRecipesFoodProcessing);
 	}
 
 	// Method to get all categories
